e8fa7518be592fba5754f86f23c9ef39d2ea5e9e
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-03-28 08:53:03 -0500

    first commit

---
+++ applications/app-service/src/main/java/radar_de_aprendiz/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package radar_de_aprendiz.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "radar_de_aprendiz.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
