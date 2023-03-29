b7c70664b60d0e058e2fee5f3531f857dab65263
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-03-29 17:13:59 -0500

    Configuracion CORS

---
+++ infrastructure/entry-points/reactive-web/src/main/java/radar_de_aprendiz/api/corsFilter.java
@@ -0,0 +1,30 @@
+package radar_de_aprendiz.api;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+import org.springframework.http.HttpMethod;
+@Component
+public class corsFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpResponse response = exchange.getResponse();
+        HttpHeaders headers = response.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+        headers.add("Access-Control-Allow-Headers", "Content-Type");
+
+        if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+            response.setStatusCode(HttpStatus.OK);
+            return Mono.empty();
+        }
+
+        return chain.filter(exchange);
+    }
+}
